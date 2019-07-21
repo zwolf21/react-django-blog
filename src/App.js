80e84b5a1c0fba5324725f9dfece1a6edc457282
HS Moon <pbr112@naver.com>
2019-07-22 00:16:04 +0900

    스토어 생성

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
 
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import configure from "./store/configure";
+
+const store = configure();
 
 function App() {
-  return <BrowserRouter>React Django Blog</BrowserRouter>;
+  return (
+    // <Provider store={store}>
+    <BrowserRouter>React Django Blog</BrowserRouter>
+    // </Provider>
+  );
 }
 
 export default App;
---
+++ src/store/modules/post.js
@@ -0,0 +1,8 @@
+import { createAction, handleActions } from "redux-actions";
+
+import { Map } from "immutable";
+import { pender } from "redux-pender";
+
+const initialState = {};
+
+export default handleActions({}, initialState);
