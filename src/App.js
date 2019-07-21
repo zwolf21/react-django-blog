b2d4988c7e7f47ca65df5e56788df6f318b3a641
HS Moon <pbr112@naver.com>
2019-07-22 00:31:34 +0900

    라우터 설정

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from "react";
 
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
+
 import configure from "./store/configure";
+import { ListPage, PostPage, EditorPage, NotFoundPage } from "./pages";
 
 const store = configure();
 
 function App() {
   return (
-    // <Provider store={store}>
-    <BrowserRouter>React Django Blog</BrowserRouter>
-    // </Provider>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ListPage} />
+          <Route path="/page/:page" component={ListPage} />
+          <Route path="/tag/:tag/:page?" component={ListPage} />
+          <Route path="/post/:id" component={PostPage} />
+          <Route path="/editor" component={EditorPage} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
