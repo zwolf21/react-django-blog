d51b0c4ce3e3b063cb0d06e42bd0b00f66978e4f
zwolf21 <pbr112@naver.com>
2019-07-22 15:52:32 +0900

    라우터 세팅

---
+++ src/pages/EditorPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EditorPage = () => {
+  return (
+    <div>
+      <div>EditorPage</div>
+    </div>
+  );
+};
+
+export default EditorPage;
---
+++ src/pages/NotFoundPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      <div>NotFoundPage</div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
